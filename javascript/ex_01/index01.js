814a0cfd5774577f853e1d01eced5b81226652c8
anithaamarnath <anithaamarnath04@gmail.com>
2021-01-29 20:10:38 -0800

    Ex_01 in Javascript homework

---
+++ javascript/ex_01/index01.js
@@ -0,0 +1,12 @@
+var first_name = 'Paw';
+var lastName = 'petrol';
+let age ='6';
+var AddressOfLocation='USA';
+var dateOfBirth ='01/01/2015'; 
+const ID=111;
+console.log("This is First Name and last name ",first_name + lastName) ;
+console.log(`This is ${first_name} of your ${age} and your dateof birth ${dateOfBirth}`);
+console.log("This is your school id never change",ID);
+
+
+
