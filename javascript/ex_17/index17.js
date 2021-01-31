9d4e22e9c41cba596a399a953644590d8cef9dee
anithaamarnath <anithaamarnath04@gmail.com>
2021-01-30 22:25:49 -0800

    Added ex_17 in javascript homework

---
+++ javascript/ex_17/index17.js
@@ -0,0 +1,7 @@
+let seriesName = 'Game of Thrones';
+let dragon1 = 'Dragon';
+let dragon2 = 'Rhaegal';
+let dragon3 = 'Viserion';
+let template = `${seriesName} the dragon name are: ${dragon1}, ${dragon2}, ${dragon3}`;
+
+console.log(template);
