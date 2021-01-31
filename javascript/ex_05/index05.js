e073021266301f00988fcd72e4f02601940f646d
anithaamarnath <anithaamarnath04@gmail.com>
2021-01-30 16:03:55 -0800

    Added ex 06  in javascript homework

---
+++ javascript/ex_05/index05.js
@@ -0,0 +1,11 @@
+let firstName = 'Dev';
+let lastName = 'Amarnath';
+let age = 05;
+let dateOfBirth = '17/17/2015';
+let address = 'surrey';
+
+console.log('This is my firstName:', firstName);
+console.log('This is my lastName:',lastName);
+console.log('This is my age',age);
+console.log('This is my date of birth', dateOfBirth);
+console.log('This is my adress',address);
