ed2af696ae34a299ce73678e328666fa38c43a97
anithaamarnath <anithaamarnath04@gmail.com>
2021-01-31 16:58:17 -0800

    Added ex_21 in javascript homework

---
+++ javascript/ex_21/index21.js
@@ -0,0 +1,14 @@
+let age = 30;
+let phoneNumber = 1234567890;
+let streetName = 'Elgin street';
+let streetNumber = 6543;
+console.log(`I'm ${age} year old`);
+console.log(`My phone number is: ${phoneNumber}`);
+console.log(`I live on ${streetNumber} and ${streetNumber}`);
+
+let age1 = `I'm ${age} year old`;
+let phoneNumber1 = `My phone number is: ${phoneNumber}`;
+let address1 = `I live on ${streetNumber} and ${streetNumber}`;
+
+console.log(age1,'\n' + phoneNumber, '\n' + address1  );
+
