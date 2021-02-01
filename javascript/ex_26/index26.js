58d732b9a391eb655efb70aff640c2f1922d7827
anithaamarnath <anithaamarnath04@gmail.com>
2021-01-31 17:44:27 -0800

    Added ex_26 in javascript homework

---
+++ javascript/ex_26/index26.js
@@ -0,0 +1,18 @@
+let canada = 'British Columbia';
+let capitial = 'Victoria';
+let cities = 52;
+let population = 3327824
+let commute = true;
+let nickName;
+let year = null;
+
+console.log(`We are in  CANADA ${canada} and CAPITIAL  ${capitial} were CITIES ${cities} and population of ${population} we can commute to any where is ${commute} and any NICKNAME ${nickName} which YEAR of CENSUS is ${year}   `);
+
+
+console.log(typeof canada );
+console.log(typeof capitial );
+console.log(typeof cities  );
+console.log(typeof population );
+console.log(typeof commute );
+console.log(typeof nickName);
+console.log(typeof year );
