df94615adf6fcf64d8f87fe26e90d7d098a7b707
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-02 15:51:39 -0800

    Added ex_32 to in javascript

---
+++ javascript/ex_32/index32.js
@@ -0,0 +1,6 @@
+let height = 10;
+let base = 4;
+let result1 = 2 * (height + base);
+let result2 = height * base;
+console.log(`The perimeter of the rectangle is ${result1}`);
+console.log(`The area of the Rectangle is ${result2}`);
