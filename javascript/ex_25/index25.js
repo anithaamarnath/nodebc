ab782e860c5612042febf077cc5303649ce49a59
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-01 19:59:19 -0800

    Added ex_23 and ex_25

---
+++ javascript/ex_25/index25.js
@@ -0,0 +1,21 @@
+let superHeroName = 'Batman';
+let name = 'Bruce Wayne';
+let fly = false;
+let life = 50;
+let freeTime = null;
+let firstName;
+console.log('Super Hero Name: ' ,superHeroName);
+console.log('Name: ', name);
+console.log('Fly: ', fly)
+console.log('Life: ',life);
+console.log('FreeTime: ', freeTime);
+
+console.log(typeof superHeroName);
+console.log(typeof name);
+console.log(typeof  fly);
+console.log(typeof life );
+console.log(typeof freeTime);
+console.log(typeof firstName);
+
+
+
