30eb4f95f940bda689f6ffed23e4ed4167098486
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-02 13:26:14 -0800

    Added ex_28 to in javascript

---
+++ javascript/ex_28/index28.js
@@ -0,0 +1,14 @@
+let myNum = 9;
+console.log(`1 * = ${myNum * 1}`);
+console.log(`2 * = ${myNum * 2}`);
+console.log(`3 * = ${myNum * 3}`);
+console.log(`4 * = ${myNum * 4}`);
+console.log(`5 * = ${myNum * 5}`);
+console.log(`6 * = ${myNum * 6}`);
+console.log(`7 * = ${myNum * 7}`);
+console.log(`8 * = ${myNum * 8}`);
+console.log(`9 * = ${myNum * 9}`);
+console.log(`10 * = ${myNum * 10}`);
+
+
+
