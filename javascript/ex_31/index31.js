d28d8a1354c13bf1a582ad3cb70523c6df8be219
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-02 15:44:28 -0800

    Added ex_30 and ex_#1 to in javascript

---
+++ javascript/ex_31/index31.js
@@ -0,0 +1,6 @@
+let l = 10;
+let h = 20;
+let b = 5;
+let result = l + h + b;
+
+console.log(`The triangle perimeter  is ${result} and sides of the perimeter is length: ${l}, height: ${b} and base${b}`);
