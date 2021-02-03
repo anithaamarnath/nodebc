db845fce725ada645affdfe2d4ba922e158fb5f3
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-02 16:36:02 -0800

    Added ex_33 to in javascript

---
+++ javascript/ex_33/index33.js
@@ -0,0 +1,27 @@
+let useCount = 100;
+useCount += 5;
+
+console.log(`This incremented by 5 ${useCount}`);
+// console.log("1.",useCount);
+
+useCount -= 3;
+console.log(`This Decremented by s ${useCount}`);
+
+
+useCount *= 2;
+console.log(`This double the company  ${useCount}`);
+
+useCount /= 2;
+console.log(`This divided  the company ${useCount} in men 50% and women 50%`);
+
+useCount %= 2;
+console.log(`This Remainder of  the company  ${useCount} `);
+
+
+
+
+
+
+
+
+
