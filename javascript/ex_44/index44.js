eabdced706449a5d77f361033a163ed3c9a39d5c
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-04 18:58:33 -0800

    Added ex_44 in javascript

---
+++ javascript/ex_44/index44.js
@@ -0,0 +1,10 @@
+let text = "HELLO";
+
+let result1 = text.charAt(0);
+let result2 = text.charAt(1);
+let result3 = text.charAt(2);
+let result4 = text.charAt(3);
+let result5 = text.charAt(4);
+let finalResult = result5.concat(result4,result3,result2,result1);
+
+console.log("result", finalResult.toLowerCase());
