90e378e40c853bb4e94163afb7bfbf656dd086d2
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-05 11:13:08 -0800

    Added ex_47

--- javascript/ex_47/index47.js
+++ javascript/ex_47/index47.js
@@ -1,3 +1,15 @@
-let students = 'Anitha, Srilakshmi, Abinaya, Abirami,Durga';
+let students = '       Anitha, Srilakshmi, Abinaya, Abirami,Durga \n';
 let result = students.split(',');
+let word = 'i';
+let searchWord = 'Abirami';
+
+
+
+
 console.log('Student Name:', result);
+console.log('Is that :',word, 'in the student list' ,students.includes(word));
+console.log('searchWord:', students.indexOf(searchWord));
+console.log('Repeate the word', students.repeat( 2));
+console.log('Replace the word', students.replace('Anitha', 'Amarnath'));
+console.log('Trim thr output', students.trim());
+
