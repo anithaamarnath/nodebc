5cccc3f947ac4743e1ea7ae662014f745f256f04
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-05 23:07:48 -0800

    Added the ex_60, ex_59

---
+++ javascript/ex_59/index59.js
@@ -0,0 +1,9 @@
+let firstNumber = 22;
+let secondNumber = 44;
+ if(firstNumber == secondNumber){
+     console.log('They are the same number');
+ }  if (firstNumber > secondNumber){
+    console.log(`${firstNumber} is bigger than ${secondNumber}`)
+ } if (firstNumber < secondNumber) {
+    console.log(`${secondNumber} is bigger than ${firstNumber}`)
+ }
---
+++ javascript/ex_60/index60.js
@@ -0,0 +1,11 @@
+let grade = 5;
+   
+if(grade >= 9){
+    console.log(`Great work!!`)
+} else 
+
+if(grade >= 6 && grade < 9){
+    console.log(`You have done great job!!`)
+} else {
+    console.log('You need to do all the exercises again, please try again!');
+}
