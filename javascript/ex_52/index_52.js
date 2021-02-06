89e1e7bec1b199422fb354cab6af40d90df1e812
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-05 16:46:59 -0800

    Addedd ex_51, to 54

---
+++ javascript/ex_52/index_52.js
@@ -0,0 +1,7 @@
+let firstNumber = 5;
+let secondNumber = 10;
+if(firstNumber > secondNumber){
+    console.log(`First Number ${firstNumber} is bigger than  Second Number ${secondNumber}`)
+} else {
+    console.log(`Second Number ${secondNumber} is bigger than  First Number ${firstNumber}`)
+}
---
+++ javascript/ex_54/index54.js
@@ -0,0 +1,7 @@
+let userName = 'pepe207';
+let password = 12345;
+if(userName == 'pepe2017' && password == 12345){
+    console.log(`Logged in User, show user home page`);
+} else {
+    console.log('Sorry, there has been a problem, please try it again.');
+}
