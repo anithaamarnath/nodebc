95a711be841e5d217049bad28a50bc2486a67412
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-05 22:25:32 -0800

    Added ex_55

---
+++ javascript/ex_55/index55.js
@@ -0,0 +1,7 @@
+let firstNumber = 5;
+let secondNumber = 10;
+let result = (firstNumber > secondNumber) ? `First Number ${firstNumber} is bigger than  Second Number ${secondNumber}`
+:`Second Number ${secondNumber} is bigger than  First Number ${firstNumber}`
+;
+
+console.log(result);
