19aff47b7ff081b3df2cf02d115c1b9cc0ef6676
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-07 23:20:56 -0800

    added ex_89 and 90 in js

---
+++ javascript/ex_89/index89.js
@@ -0,0 +1,13 @@
+
+
+let sum =0;
+for (num = 0;num <= 1000; num++){
+    
+    if(num % 2 == 0){
+        console.log(num);
+       
+        sum += num; 
+        console.log('Sum',sum);
+    }
+    
+}
---
+++ javascript/ex_90/index90.js
@@ -0,0 +1,11 @@
+
+let sum = null;
+for(num=0; num <= 1000; num++){
+    if(num % 2 !== 0){
+        console.log(num);
+        sum += num; 
+        console.log('Sum',sum);
+    }
+  
+    
+}
