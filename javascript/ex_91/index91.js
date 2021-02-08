6240eed399586df882ab6a47f6cf27c07bdc22e8
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-07 23:25:04 -0800

    added ex_91 to 92

---
+++ javascript/ex_91/index91.js
@@ -0,0 +1,6 @@
+
+for (let time = 1; time <= 10; time++){
+   console.log(`9 times ${time} = ${9 * time} `)
+   
+    
+}
---
+++ javascript/ex_92/index92.js
@@ -0,0 +1,8 @@
+
+let star ='*';
+for (let num = 0; num <= 10;num++){
+        
+        star += '*';
+        console.log(star);     
+       
+}
