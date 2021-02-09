a011901e504a65d405604e9efe17c52a90412a50
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-08 18:13:29 -0800

    Added ex_100 and ex_101 in JS

---
+++ javascript/ex_100/index100.js
@@ -0,0 +1,11 @@
+
+
+function evenNumber(){
+    for (num = 0;num <= 100; num++){   
+        if(num % 2 == 0){
+        console.log('Even Number',num);
+        }   
+    }
+}
+
+evenNumber();
---
+++ javascript/ex_101/index101.js
@@ -0,0 +1,12 @@
+function showUser() {
+let name = 'Anitha';
+let age = 30;
+let phoneNumber=1234567890;
+let street = 'elgin';
+let postalCode = 'v5w 3k4';
+let married = true;
+console.log(`Name:${name}\nAge:${age}\nPhoneNumber:${phoneNumber}\nStreet:${street}\nPostalCode:${postalCode}\nMarried:${married}`);
+
+}
+
+showUser();
