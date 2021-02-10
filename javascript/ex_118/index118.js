7f2973d85a5f403cd7de12195df666b4fce6dd07
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-09 21:33:54 -0800

    Added ex_120

---
+++ javascript/ex_118/index118.js
@@ -0,0 +1,8 @@
+let grades = [1,2,3,4,5,6,7,8,9,10]
+let cGrade  = grades[0];
+let bGrade = grades[3];
+let aGrade = grades[9];
+
+console.log(cGrade);
+console.log(bGrade);
+console.log(aGrade);
---
+++ javascript/ex_120/index120.js
@@ -0,0 +1,8 @@
+let students = [];
+students[0] = 'Anitha'  ;
+students[1] = 'Abi';
+students[2] = 'Abirami';
+students[3] = 'Dev';
+students[4] = 'Srilakshmi';
+
+console.log(students[0]);
