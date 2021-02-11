e2038c65ab34d6f0092ab67e5f640db28de6e43c
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-11 10:27:47 -0800

    Added ex_126

---
+++ javascript/ex_126/index126.js
@@ -0,0 +1,14 @@
+const iceCream = [];
+
+iceCream.unshift('mango flavour');
+iceCream.unshift('orange flavour');
+iceCream.unshift('pine apple flavour');
+iceCream.unshift('vanilla flavour');
+iceCream.unshift('berry flavour');
+iceCream.unshift('straberry flavour');
+iceCream.unshift('cranberry flavour');
+iceCream.unshift('coke flavour');
+iceCream.unshift('blueberry flavour');
+iceCream.unshift('fruits flavour');
+
+console.log(iceCream);
