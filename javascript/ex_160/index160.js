4458950affc22301595f776d53697a121ca434e0
anithaamarnath <anithaamarnath04@gmail.com>
2021-02-16 12:34:33 -0800

    Added ex_160 to 162

---
+++ javascript/ex_160/index160.js
@@ -0,0 +1,102 @@
+const propertiesName = [
+
+    'name',
+    'price',
+    'sold',
+    'console'
+    
+
+]
+
+
+const games = [];
+
+
+games.push({
+    'name': 'Crash Bandicoot N. Sane Trilogy',
+    'price': 1060,
+    'sold': 20,
+    'console': 'PS4'
+})
+games.push({'name': 'Lego Marvel Super Heroes',
+'price': 700,
+'sold': 25,
+'console': 'XBOX'});
+
+games.push({'name': 'Gta V',
+'price': 1449,
+'sold': 30,
+'console': 'PS4',
+})
+
+games.push({'name': 'Mortal Kombat Xl',
+'price': 1190,
+'sold': 34,
+'console': 'PS4'
+})
+games.push({
+'name': 'Gta V',
+'price': 1250,
+'sold': 60,
+'console': 'XBOX'
+});
+games.push({
+'name': 'Fifa 2017',
+'price': 890,
+'sold': 15,
+'console': 'PS4'
+})
+games.push({'name': 'Uncharted 4',
+'price': 950,
+'sold': 30,
+'console': 'PS4'
+})
+
+games.push({'name': 'Mortal Kombat Xl',
+'price': 940,
+'sold': 30,
+'console': 'XBOX'
+})
+
+games.push({'name': 'Need For Speed',
+'price': 790,
+'sold': 10,
+'console': 'PS4'
+})
+games.push({
+'name': 'Lego Batman',
+'price': 1000,
+'sold': 18,
+'console':'PS4'
+})
+
+games.push({
+
+'name': 'Fifa 17',
+'price': 1290,
+'sold': 12,
+'console': 'PS4'
+})
+
+games.push({
+'name': 'Resident Evil 7',
+'price': 1390,
+'sold': 10,
+'console': 'PS4'
+})
+games.push({
+'name': 'Dragon Ball XX',
+'price': 1390,
+'sold': 25,
+'console': 'XBOX'
+})
+ games.map(obj => {
+   
+   console.log(`name: ${obj[propertiesName[0]]}`);
+   console.log(`price: ${obj[propertiesName[1]]}`);
+   console.log(`sold: ${obj[propertiesName[2]]}`);
+   console.log(`console: ${obj[propertiesName[3]]}`);
+
+})
+
+
