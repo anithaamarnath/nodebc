a605e5543df54f1c09809d1c8199c0e1c777d951
anithaamarnath <anithaamarnath04@gmail.com>
2021-03-09 16:46:53 -0800

    Added ex_5

---
+++ node-js/ex_5/index.js
@@ -0,0 +1,3 @@
+const isEven = require('./numbers.js')
+const even = isEven(20)
+console.log(even);
---
+++ node-js/ex_5/numbers.js
@@ -0,0 +1,11 @@
+const isEven = function(number){
+    if(number % 2 === 0){
+       console.log(`the given ${number} is even`)
+       
+    } else {
+        console.log(`the given is odd`)
+    }
+   
+}
+  
+module.exports = isEven;
