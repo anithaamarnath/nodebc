9a72bf5b013459f124fc3cbcbc3f5c4c60488818
anithaamarnath <anithaamarnath04@gmail.com>
2021-03-11 23:03:00 -0800

    Ex_6

---
+++ node-js/ex_6/index.js
@@ -0,0 +1,11 @@
+const greet = (firstName, lastName, message) => {
+    console.log(`Hi ${firstName} ${lastName}`);
+    message()
+   
+}
+const message = () => {
+    console.log(`This code gets executed after the greet function call`)
+}
+
+
+greet('Anita','Amarnath',message);
